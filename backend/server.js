0f7b0aa32aea15b8699ae6eebbe6d5739637e7f4
mamite100 <mamite.andersson@student.kyh.se>
2022-01-26 07:27:34 +0100

    modefied code backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,34 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 mongoose.Promise = Promise
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -21,7 +45,36 @@ app.use(express.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
-})
+});
+
+app.post('/signup', async (req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        accessToken: newUser.accessToken,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
