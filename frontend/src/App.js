f525f075a7b158e01cc12147e4df69b3008f0efa
mamite100 <mamite.andersson@student.kyh.se>
2022-01-31 22:48:15 +0100

    set up logic for login, unfortunatly it does not work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore , combineReducers} from '@reduxjs/toolkit';
 import {
   BrowserRouter,
   Routes,
@@ -7,19 +9,27 @@ import {
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
-import UserProfile from './pages/UserProfile'
+import UserProfile from './pages/UserProfile';
 
+import user from './reducers/user';
 
+const reducer = combineReducers({
+  user: user.caseReducers
+});
+const store = configureStore({reducer}); 
+  
 export const App = () => {
   return (
 
     <BrowserRouter>
+    <Provider store ={store}>
     <Routes>
         <Route path ="/" element = {<Home/> } />
         <Route path ="/login" element = {<Login/> }/>
         <Route path ="/userProfile"  element = {<UserProfile/> }/>
         <Route path ="*" element = {<NotFound/> } />
     </Routes>
+    </Provider>
     </BrowserRouter>
   ); 
 };
--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,41 +1,13 @@
 import React from "react";
 import styled from 'styled-components/macro';
-import Lottie from 'lottie-react';
-import animationData from '../lotties/404-error.json'; 
 import { useNavigate } from "react-router-dom";
 
 import Button from "../components/Button";
 
-const NotFoundContainer = styled.footer `
-width: 80%
-height: auto;
-display: flex;
-margin:0 auto;
-flex-direction: column; 
-background-color: pink;
-align-items: center; 
-`;
+
 
 const NotFound = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
-    const navigate = useNavigate();
-    function handleClick() {
-        navigate("/");
-    };
-    return (
-
-        <NotFoundContainer>
-           <Button onClick={handleClick} buttonText={"Return Home"}></Button>
-            <Lottie options={defaultOptions} maxHeight={100} maxWidth={100} />
-        </NotFoundContainer>
-    );
-};
+    return <h1>The page is not found</h1>
+} ;
 
 export default NotFound;
